2e97eea691845ab0be64845de45805cfab991def
Bimbo Escodero Villaganas <villaganasbimbo143@gmail.com>
2024-07-14 19:32:15 +0800

    first commit

---
+++ index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="assets/images/logo.png" type="image/x-icon">
+
+    <!--Bootsrap CSS--><link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="src/css/index.css">
+
+    <title>Household Profiling</title>
+
+</head>
+<body>
+<div class="landing-page">
+        <header>
+        <div class="container">
+  <a href="#" class="logo">A Household Profiling System for <b>
+    <span class="red">A</span><span class="green">L</span><span class="blue">S</span>
+  </b></a>
+  <ul class="links">
+    <li>Home</li>
+    <li>About Us</li>
+    <li>Work</li>
+    <li>Info</li>
+    <li>Get Started</li>
+  </ul>
+</div>
+
+        </header>
+        <div class="content">
+          <div class="container">
+            <div class="info">
+              <h1>Mapping the Path to Learning: The ALS Household Profiling System for Barangay Tankulan</h1>
+              <p>The ALS Household Profiling System is a vital tool for identifying and supporting out-of-school youth in Barangay Tankulan. This innovative system leverages data analytics to create detailed profiles of each household, revealing the unique needs and circumstances of young people who are not currently enrolled in traditional educational settings. By understanding the specific challenges, aspirations, and potential barriers faced by these individuals, the ALS program can tailor its resources and interventions to effectively reach and empower out-of-school youth in Barangay Tankulan.</p>
+              <button>Button name</button>
+            </div>
+            <div class="image">
+              <img src="assets/images/alslanding.jpg">
+            </div>
+          </div>
+        </div>
+      </div>
+      
+    
+</body>
+
+<!--Bootsrap js-->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+</html>
